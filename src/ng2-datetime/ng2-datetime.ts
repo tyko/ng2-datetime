0cb878da6a5a2663793adc0a6793a91c4623f10d
shoudaos <shoudaos92@gmail.com>
2016-06-09 15:49:59 +0300

    Fix destroy method not found

--- src/ng2-datetime/ng2-datetime.ts
+++ src/ng2-datetime/ng2-datetime.ts
@@ -50,7 +50,7 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
 
     ngOnDestroy() {
         if (this.datepicker) {
-            this.datepicker.destroy();
+            this.datepicker.data().datepicker.destroy();
         }
     }
 
